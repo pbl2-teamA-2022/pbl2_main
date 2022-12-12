c71c7b0ed3e5dd159074a14b8aed555692f02198
g428miyo <miyoshiyuriko1@gmail.com>
2022-12-12 11:45:09 +0900

    Update home.php

--- home.php
+++ home.php
@@ -23,6 +23,7 @@
   </style>
   <script type="text/javascript">
     var server = "/home/g428miyo/public_html/pbl2";
+    var file_name = "home.php";
     function make_group_selector(name, position){
       _d = new Date().getTime(); //キャッシュ回避のため日時を利用する
       $.get("function_list/search_group.php?"
